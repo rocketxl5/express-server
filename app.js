8ec0c42aeedc0fd9c5cc41ce73682be64ab63cd7
Max Gaillard <maxgaillar@gmail.com>
2023-08-30 15:46:17 -0400

    Added post request

--- app.js
+++ app.js
@@ -14,4 +14,14 @@ app.get('/', async (req, res) => {
     }
 })
 
+app.post('/', async (req, res) => {
+    try {
+        const data = await req.body
+
+        res.send(201).json({ data })
+    } catch (error) {
+        res.send(500).json({ error: error.message })
+    }
+})
+
 app.listen(PORT, () => console.log`Server running on port ${PORT}`)
