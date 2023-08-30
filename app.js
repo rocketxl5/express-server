fd5674e43932b7d392fd56f476a1d2a7e1dfab80
Max Gaillard <maxgaillar@gmail.com>
2023-08-30 16:02:39 -0400

    Modified post code

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', async (req, res) => {
     try {
         res.status(201).json({ message: "hello world" })
     } catch (error) {
-        console.log(error.message)
+        res.send(500).json({ message: message.error })
     }
 })
 
