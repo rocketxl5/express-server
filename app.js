533434077b3b8617799fc13ffa1e5dde1acbb8a6
Max Gaillard <maxgaillar@gmail.com>
2023-08-30 15:40:49 -0400

    Minor change on response

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 
 app.get('/', async (req, res) => {
     try {
-        res.status(201)._construct({message: "hello world"})
+        res.status(201).json({ message: "hello world" })
     } catch (error) {
         console.log(error.message)
     }
